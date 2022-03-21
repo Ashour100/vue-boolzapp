6758614df05130bcd2fa2c38031dbd677e1d8118
Ashour100 <interinstitute5@gmail.com>
2022-03-21 16:06:27 +0100

    Messages and time updated

--- main.js
+++ main.js
@@ -12,17 +12,20 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:50'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        time:'16:15'
                     }
                 ],
             },
@@ -34,17 +37,20 @@ const app= new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'16:30'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        time:'16:30'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'16:35'
                     }
                 ],
             },
@@ -56,17 +62,20 @@ const app= new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        time:'10:10'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'10:20'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        time:'16:15'
                     }
                 ],
             },
@@ -78,12 +87,14 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:50'
                     }
                 ],
             },
@@ -95,12 +106,14 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:50'
                     }
                 ],
             },
@@ -112,17 +125,20 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:50'
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:51'
                     }
                 ],
             },
@@ -134,12 +150,14 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:30'
                     }
                 ],
             },
@@ -151,17 +169,20 @@ const app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:30'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        time:'15:50'
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        time:'15:51'
                     }
                 ],
             }
@@ -169,5 +190,9 @@ const app= new Vue({
         
     },
     methods:{
+        // test: function(){
+        //     for(element in this.contacts)
+        //     console.log(element.messages.lenght);
+        // }
     }
 });
